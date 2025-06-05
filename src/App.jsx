c3c3d132c240efc6d1cc3fa2360f41c3569f8fa9
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-06-05 14:41:25 +0530

    story created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -10,6 +11,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Features/>
+      <Contact/>
     </main>
   );
 };
